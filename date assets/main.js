7228a526ef26bd8bd67020b7b9496cd75ef663fc
Ethan Sheffield <ethanbsheffield@gmail.com>
2024-03-28 10:37:34 -0500

    minor changes

--- date assets/main.js
+++ date assets/main.js
@@ -1,44 +1,3 @@
-// const container = document.getElementById("container");
-// const imageOne = document.querySelector(".image-1");
-// const imageTwo = document.querySelector(".image-2");
-// const btnYes = document.querySelector(".btn-yes");
-// const btnNo = document.querySelector(".btn-no");
-
-// function getRandomNumber(min, max) {
-//   // Calculate the random number between min and max (inclusive)
-//   const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-
-//   return randomNumber;
-// }
-
-// btnNo.addEventListener("mouseover", (event) => {
-//   const containerHeight = container.getBoundingClientRect().height;
-//   const containerWidth = container.getBoundingClientRect().width;
-//   const btnHeight = btnNo.getBoundingClientRect().height;
-//   const btnWidth = btnNo.getBoundingClientRect().width;
-//   const btnTop = btnNo.getBoundingClientRect().top;
-//   const btnLeft = btnNo.getBoundingClientRect().left;
-
-//   let newTop = btnTop;
-//   let newLeft = btnLeft;
-//   while (Math.abs(newTop - btnTop) < containerHeight / 3) {
-//     newTop = getRandomNumber(0, containerHeight - btnHeight);
-//   }
-
-//   while (Math.abs(newLeft - btnLeft) < containerWidth / 3) {
-//     newLeft = getRandomNumber(0, containerWidth - btnWidth);
-//   }
-
-//   btnNo.style.top = Math.floor(newTop) + "px";
-//   btnNo.style.left = Math.floor(newLeft) + "px";
-// });
-
-// btnYes.addEventListener("click", (e) => {
-//   btnNo.classList.add("hide");
-//   imageOne.classList.add("hide");
-//   imageTwo.classList.remove("hide");
-// });
-
 const container = document.getElementById("container");
 const headerImg = document.querySelector('.header-img'); // Select the header image
 const imageOne = document.querySelector(".image-1");
@@ -64,8 +23,7 @@ function moveButton() {
   btnNo.style.left = `${newLeft}px`;
 }
 
-// Set the button to move every 2 seconds
-setInterval(moveButton, 750);
+setInterval(moveButton, 700);
 
 btnYes.addEventListener("click", (e) => {
   btnNo.classList.add("hide");
